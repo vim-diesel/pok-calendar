5dc287a82e8f6eb9ee9d00e2b4c3029006b7b323
Ian C <ian.clark@hotmail.com>
2024-10-01 18:45:46 -0400

    added typography to meeting descriptions

--- app/MeetingCard.tsx
+++ app/MeetingCard.tsx
@@ -4,15 +4,15 @@ import { ParsedDescription } from './ParsedDescription';
 
 export default function MeetingCard({ meeting }: { meeting: Meeting }) {
   return (
-    <div className='divide-y divide-gray-200 rounded-lg bg-white shadow'>
-      <div className='px-4 py-5 sm:px-6'>
+    <div className='m-6 border-2 rounded-lg bg-white shadow'>
+      <div className='px-4 py-5 sm:px-6 border-b-2'>
         {/* We use less vertical padding on card headers on desktop than on body sections */}
         {meeting.title}
       </div>
-      <div className='px-4 py-5 sm:p-6 max-h-64 overflow-auto'>
+      <div className='bg-slate-100 font-mono italic px-4 py-5 sm:p-6 max-h-64 overflow-auto'>
         <ParsedDescription description={meeting.description} />
       </div>
-      <Host {...meeting.host} />
+      {meeting.host && <Host {...meeting.host} />}
     </div>
   );
 }
--- app/ParsedDescription.tsx
+++ app/ParsedDescription.tsx
@@ -1,8 +1,12 @@
 import parse from 'html-react-parser';
 
-export const ParsedDescription = ({ description }: { description: string | null | undefined }) => {
+export const ParsedDescription = ({
+  description,
+}: {
+  description: string | null | undefined;
+}) => {
   if (!description) {
     return <div></div>;
   }
-  return <div>{parse(description)}</div>;
+  return <article className='prose prose-a:text-blue-600 prose-ul:list-none prose-ul:list-outside prose-ul:m-0 prose-li:m-0 max-w-none'>{parse(description)}</article>;
 };
